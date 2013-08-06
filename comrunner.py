63544f6b51fa3a4b759ddb25296f7254dfafc1ec
Guillaume Dugas <dugas.guillaume@gmail.com>
2013-08-06 10:13:01 +0200

    ComRunner: A commandline utility

---
+++ comrunner.py
@@ -0,0 +1,61 @@
+
+class Argument(object):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+class CommandRunner(object):
+    
+    description = ""
+    args = []
+    subcommands = {}
+    
+    
+    def create_parser(self):
+        import argparse
+        return argparse.ArgumentParser(description = self.description)
+    
+    
+    def init_parser(self, parser=None):
+        if not parser:
+            parser = self.create_parser()
+        
+        for arg in self.args:
+            if not isinstance(arg, Argument):
+                raise Exception("Not an Argument instance")
+            parser.add_argument(*arg.args, **arg.kwargs)
+        
+        
+        if len(self.subcommands) == 0:
+            parser.set_defaults(func=self.execute)
+            return parser
+        
+        subparsers = parser.add_subparsers()
+        for k in self.subcommands:
+            if not issubclass(self.subcommands[k], CommandRunner):
+                raise Exception("Not a CommandRunner subclass")
+            cmd = self.subcommands[k]()
+            
+            subparser = subparsers.add_parser(k, help=cmd.description)
+            cmd.init_parser(subparser)
+        
+        return parser
+    
+    
+    def parse_args(self, args=None):
+        parser = self.init_parser()
+        if args == None:
+            return parser.parse_args()
+        else:
+            return parser.parse_args(args)
+    
+    
+    def run(self, args=None):
+        args = self.parse_args(args)
+        args.func(args)
+    
+    
+    def execute(self, args):
+        m = "Undefined abstract method 'handle()'"
+        raise NotImplementedError(m)
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ComRunner',
+      version='0.1',
+      description='Commandline runner utility',
+      author='Guillaume Dugas',
+      author_email='dugas.guillaume@gmail.com',
+      py_modules=['comrunner']
+     )
